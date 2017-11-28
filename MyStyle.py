609e5427fb81cb2d9f5e0b82b418ab1551aab4a3
Robin Aggleton <robin.aggleton@cern.ch>
2017-11-28 11:29:23 +0100

    Move axes title further away

--- MyStyle.py
+++ MyStyle.py
@@ -63,8 +63,8 @@ My_Style.SetTitleFont(42, "XYZ")
 My_Style.SetTitleSize(0.04, "XYZ")
 # My_Style.SetTitleXSize(Float_t size = 0.02) # Another way to set the size?
 # My_Style.SetTitleYSize(Float_t size = 0.02)
-My_Style.SetTitleXOffset(1.05)
-My_Style.SetTitleYOffset(1.4)
+My_Style.SetTitleXOffset(1.25)
+My_Style.SetTitleYOffset(1.45)
 
 # For the statistics box:
 My_Style.SetOptFile(0)
