d7c8ee18599be03c38ee5c56d39613752d885c75
Robin Aggleton <robin.aggleton@cern.ch>
2017-11-29 12:06:52 +0100

    Fix output dir typo

--- doPythiaHerwigComparisons.sh
+++ doPythiaHerwigComparisons.sh
@@ -11,5 +11,5 @@ set -u
 ./compareFlavourResponseGraphs.py \
 --input jra_QCD_FLAT_withL1L2L3_Summer16_03Feb2017_V8_f_g.root --label "PYTHIA8" \
 --input jra_QCD_FLAT_HERWIG_withL1L2L3_Summer16_03Feb2017_V8_ak4pfchs_newFlav_f_g.root --label "HERWIG++" \
---outputDir QCD_FLAT_HERWIG_withL1L2L3_Summer16_03Feb2017_V8NoJEC/comparisonPythiaHerwig \
+--outputDir QCD_FLAT_HERWIG_withL1L2L3_Summer16_03Feb2017_V8/comparisonPythiaHerwig \
 --title "Summer_03Feb2017_V8"
