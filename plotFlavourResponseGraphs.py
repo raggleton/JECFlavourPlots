f5f727c7ffa7ba598e5f0e8320165ad8824ccd5d
Robin Aggleton <robin.aggleton@cern.ch>
2018-08-02 17:45:11 +0200

    Use comparator in plotFlavourResponseGraphs to include ratio plot

--- plotFlavourResponseGraphs.py
+++ plotFlavourResponseGraphs.py
@@ -3,6 +3,7 @@
 """Plot response & resolution graphs using ROOT file from output of jet_response_and_resolution_x"""
 
 
+from __future__ import print_function
 import os
 import sys
 import argparse
@@ -14,7 +15,7 @@ import numpy as np
 import ROOT
 from MyStyle import My_Style
 import common_utils as cu
-
+from comparator import Contribution, Plot
 
 ROOT.PyConfig.IgnoreCommandLineOptions = True
 ROOT.gROOT.SetBatch(1)
@@ -40,9 +41,10 @@ def construct_inverse_graph(graph):
 def do_comparison_graph(entries, output_filename, bin_title="", xtitle="", ytitle="",
                         other_elements=None, logx=False, logy=False,
                         do_line=True, xlimits=None, ylimits=None,
-                        y_limit_protection=None, draw_fits=True):
+                        y_limit_protection=None, draw_fits=True, 
+                        do_ratio=True, ratio_limits=None):
     """Draw several graphs on one canvas and save to file
-
+    
     Parameters
     ----------
     entries : [dict]
@@ -72,78 +74,68 @@ def do_comparison_graph(entries, output_filename, bin_title="", xtitle="", ytitl
         Set minimum and maximum y values in the event of a huge stat error or weird point
     draw_fits : bool, optional
         Draw fitted functions or not
-
+    do_ratio : bool, optional
+        Add ratio subplot
+    
     """
-    mg = ROOT.TMultiGraph()
-    mg.SetTitle(";".join(["", xtitle, ytitle]))
+
+    plot = Plot(entries, what='graph', 
+                title=None, xtitle=xtitle, ytitle=ytitle,
+                xlim=xlimits, ylim=ylimits,
+                legend=True,
+                subplot=entries[0] if do_ratio else None, subplot_type="ratio",
+                subplot_title="Ratio to All")
+
+    # replace legend with our own for now
     delta = 0.12
     middle = 0.77
-    leg = ROOT.TLegend(middle-delta, 0.75, middle+delta, 0.88)
-    leg.SetBorderSize(0)
-    leg.SetFillStyle(0)
-    leg.SetNColumns(2)
-    leg.SetTextAlign(ROOT.kHAlignCenter + ROOT.kVAlignCenter)
+    plot.legend = ROOT.TLegend(middle-delta, 0.75, middle+delta, 0.88)
+    plot.legend.SetBorderSize(0)
+    plot.legend.SetFillStyle(0)
+    plot.legend.SetNColumns(2)
+    plot.legend.SetTextAlign(ROOT.kHAlignCenter + ROOT.kVAlignCenter)
 
-    for entry in entries:
-        default_colour = ROOT.kBlack
-        graph = entry['graph']
-        graph.SetLineColor(entry.get('line_color', default_colour))
-        graph.SetLineStyle(entry.get('line_style', 1))
-        graph.SetLineWidth(entry.get('line_width', 1))
-
-        graph.SetMarkerColor(entry.get('marker_color', default_colour))
-        graph.SetMarkerStyle(entry.get('marker_style', 1))
-        graph.SetMarkerSize(entry.get('marker_size', 1))
-
-        if graph.GetListOfFunctions().GetSize() > 0:
-            func = graph.GetListOfFunctions().Last()
-            if draw_fits:
-                func.SetLineColor(entry.get('line_color', default_colour))
-                func.SetLineStyle(3)
-            else:
-                # Broken for now
-                graph.GetListOfFunctions().Remove(func)
-
-        mg.Add(graph)
-        leg.AddEntry(graph, entry.get('label', graph.GetName()), "LP")
-
-    canv = ROOT.TCanvas(ROOT.TUUID().AsString(), "", 800, 800)
-    canv.SetTicks(1, 1)
+    plot.plot()
     if logx:
-        canv.SetLogx()
+        plot.set_logx()
     if logy:
-        canv.SetLogy()
-    mg.Draw("ALP")
+        plot.set_logy()
 
-    # Little extra breathing room
-    mg.GetHistogram().SetMaximum(mg.GetYaxis().GetXmax() * 1.03)
+    plot.main_pad.cd()
+    if not ylimits:
+        plot.container.GetHistogram().SetMaximum(plot.container.GetYaxis().GetXmax() * 1.05)
 
     # Protection in case y limits are dominated by large stat error
     if y_limit_protection and len(y_limit_protection) == 2:
-        y_min, y_max = mg.GetYaxis().GetXmin(), mg.GetYaxis().GetXmax()
+        y_min, y_max = plot.container.GetYaxis().GetXmin(), plot.container.GetYaxis().GetXmax()
         y_lim_lower, y_lim_upper = y_limit_protection
         if y_max > y_lim_upper:
-            mg.GetHistogram().SetMaximum(y_lim_upper)
+            plot.container.GetHistogram().SetMaximum(y_lim_upper)
         if y_min < y_lim_lower:
-            mg.GetHistogram().SetMinimum(y_lim_lower)
+            plot.container.GetHistogram().SetMinimum(y_lim_lower)
 
-    if xlimits and len(xlimits) == 2:
-        mg.GetXaxis().SetLimits(*xlimits)
+    # add protection for subplot
+    if not ratio_limits:
+        low_lim = 0.8
+        upper_lim = 1.2
+        y_min, y_max = plot.subplot_container.GetYaxis().GetXmin(), plot.subplot_container.GetYaxis().GetXmax()
+        plot.subplot_container.GetYaxis().SetRangeUser(max(low_lim, y_min), min(y_max, upper_lim))  # set limits doesn't work for y axis
+    elif len(ratio_limits) == 2:
+        plot.subplot_container.GetYaxis().SetRangeUser(*ratio_limits)  # set limits doesn't work for y axis
 
-    if ylimits and len(ylimits) == 2:
-        mg.GetHistogram().SetMaximum(ylimits[1])
-        mg.GetHistogram().SetMinimum(ylimits[0])
+    plot.subplot_pad.Update()
+    plot.canvas.Update()
 
-    leg.Draw()
+    # if do_line:
+    #     y_min, y_max = plot.container.GetYaxis().GetXmin(), plot.container.GetYaxis().GetXmax()
+    #     if y_min < 1 and y_max > 1:
+    #         x_min, x_max = plot.container.GetXaxis().GetXmin(), plot.container.GetXaxis().GetXmax()
+    #         line = ROOT.TLine(x_min, 1, x_max, 1)
+    #         line.SetLineStyle(2)
+    #         line.SetLineColor(ROOT.kGray+2)
+    #         line.Draw()
 
-    if do_line:
-        y_min, y_max = mg.GetYaxis().GetXmin(), mg.GetYaxis().GetXmax()
-        if y_min < 1 and y_max > 1:
-            x_min, x_max = mg.GetXaxis().GetXmin(), mg.GetXaxis().GetXmax()
-            line = ROOT.TLine(x_min, 1, x_max, 1)
-            line.SetLineStyle(2)
-            line.SetLineColor(ROOT.kGray+2)
-            line.Draw()
+    plot.canvas.cd()
 
     cms_text = ROOT.TPaveText(0.17, 0.84, 0.2, 0.85, "NDC")
     cms_text.AddText("CMS Simulation")
@@ -167,7 +159,7 @@ def do_comparison_graph(entries, output_filename, bin_title="", xtitle="", ytitl
         for ele in other_elements:
             ele.Draw()
 
-    canv.SaveAs(output_filename)
+    plot.save(output_filename)
 
 
 def main(in_args):
@@ -189,6 +181,66 @@ def main(in_args):
         {"flav": "b_RRspVsRefPt_RelRsp", "label": "b", "colour": ROOT.kOrange-3, "marker_style": ROOT.kFullDiamond, "line_style": 1, "line_width": lw, "marker_size": 1.6},
         {"flav": "g_RRspVsRefPt_RelRsp", "label": "g", "colour": ROOT.kAzure+1, "marker_style": 29, "line_style": 1, "line_width": lw, "marker_size": 1.8},
     ]
+
+    # no JEC
+    eta_bin_limits = {
+        'JetEta0to0.783': (0.9, 1.15),
+        'JetEta0.783to1.305': None,
+        'JetEta1.305to1.93': (0.85, 1.05),
+        'JetEta1.93to2.5': None,
+        'JetEta2.5to2.964': None,
+        'JetEta2.964to3.139': None,
+        'JetEta3.139to5.191': (0.98, 1.8),
+    }
+    eta_bin_ratio_limits = {
+        'JetEta0to0.783': (0.96, 1.07),
+        'JetEta0.783to1.305': None,
+        'JetEta1.305to1.93': (0.95, 1.07),
+        'JetEta1.93to2.5': None,
+        'JetEta2.5to2.964': None,
+        'JetEta2.964to3.139': None,
+        'JetEta3.139to5.191': (0.93, 1.1),
+    }
+    
+    # with L1 JEC
+    # eta_bin_limits = {
+    #     'JetEta0to0.783': (0.76, 1.05),
+    #     'JetEta0.783to1.305': (0.77, 1),
+    #     'JetEta1.305to1.93': (0.7, 1.),
+    #     'JetEta1.93to2.5': (0.68, 1.1),
+    #     'JetEta2.5to2.964': (0.55, 1.1),
+    #     'JetEta2.964to3.139': (0.6, 1.1),
+    #     'JetEta3.139to5.191': (0.75, 1.1),
+    # }
+    # eta_bin_ratio_limits = {
+    #     'JetEta0to0.783': (0.96, 1.08),
+    #     'JetEta0.783to1.305': (0.96, 1.08),
+    #     'JetEta1.305to1.93': (0.95, 1.09),
+    #     'JetEta1.93to2.5': (0.95, 1.13),
+    #     'JetEta2.5to2.964': (0.93, 1.15),
+    #     'JetEta2.964to3.139': (0.9, 1.12),
+    #     'JetEta3.139to5.191': (0.93, 1.1),
+    # }
+
+    # default
+    # eta_bin_limits = {
+    #     'JetEta0to0.783': None,
+    #     'JetEta0.783to1.305': None,
+    #     'JetEta1.305to1.93': None,
+    #     'JetEta1.93to2.5': None,
+    #     'JetEta2.5to2.964': None,
+    #     'JetEta2.964to3.139': None,
+    #     'JetEta3.139to5.191': None,
+    # }
+    # eta_bin_ratio_limits = {
+    #     'JetEta0to0.783': None,
+    #     'JetEta0.783to1.305': None,
+    #     'JetEta1.305to1.93': None,
+    #     'JetEta1.93to2.5': None,
+    #     'JetEta2.5to2.964': None,
+    #     'JetEta2.964to3.139': None,
+    #     'JetEta3.139to5.191': None,
+    # }
 
 
     # Loop through all different ak4pfchs, etc
@@ -229,53 +281,73 @@ def main(in_args):
         obj_list = cu.get_list_of_objects_in_dir(args.input, mydir)
 
         # Do all flavs rsp vs pt for given eta bin
-        common_eta_bins = cu.get_common_eta_bins(obj_list)
-        for eta_bin in common_eta_bins:
+        common_eta_bins = cu.sort_human(cu.get_common_eta_bins(obj_list))
+        for eta_bin in common_eta_bins[:]:
             entries = []
+            contributions = []
             for fdict in entry_dicts:
                 entry = deepcopy(fdict)
-                entry["graph"] = cu.grab_obj_from_file(args.input, "%s/%s_%s" % (mydir, fdict['flav'], eta_bin))
+                # entry["graph"] = cu.grab_obj_from_file(args.input, "%s/%s_%s" % (mydir, fdict['flav'], eta_bin))
+                entry["obj"] = cu.grab_obj_from_file(args.input, "%s/%s_%s" % (mydir, fdict['flav'], eta_bin))
                 entry["line_color"] = fdict['colour']
                 entry["marker_color"] = fdict['colour']
-                entries.append(entry)
+                # entries.append(entry)
+
+                del entry['flav']
+                del entry['colour']
+                this_contrib = Contribution(**entry)
+                contributions.append(this_contrib)
+
             bin_title = eta_bin.replace("to", " < |#eta| < ").replace("JetEta", "")
-            do_comparison_graph(entries, bin_title=bin_title,
+            do_comparison_graph(contributions, bin_title=bin_title,
                                 xtitle="p_{T}^{Gen} [GeV]", ytitle="Response", logx=True,
-                                xlimits=(10, 5000), y_limit_protection=(0.5, 1.5),
+                                xlimits=(10, 5000), 
+                                ylimits=eta_bin_limits.get(eta_bin, None),
+                                y_limit_protection=(0.5, 1.7),
                                 other_elements=other_elements,
-                                output_filename=os.path.join(plot_dir, "rsp_vs_pt_%s.pdf" % (eta_bin)))
+                                output_filename=os.path.join(plot_dir, "rsp_vs_pt_%s.pdf" % (eta_bin)),
+                                ratio_limits=eta_bin_ratio_limits.get(eta_bin, None)
+                                )
 
             # Inverse response ie ~ correction
-            entries = []
-            for fdict in entry_dicts:
-                entry = deepcopy(fdict)
-                entry["graph"] = construct_inverse_graph(cu.grab_obj_from_file(args.input, "%s/%s_%s" % (mydir, fdict['flav'], eta_bin)))
-                entry["line_color"] = fdict['colour']
-                entry["marker_color"] = fdict['colour']
-                entries.append(entry)
-            bin_title = eta_bin.replace("to", " < |#eta| < ").replace("JetEta", "")
-            do_comparison_graph(entries, bin_title=bin_title,
-                                xtitle="p_{T}^{Gen} [GeV]", ytitle="1/Response", logx=True,
-                                y_limit_protection=(0.8, 1.2),
-                                other_elements=other_elements,
-                                output_filename=os.path.join(plot_dir, "inv_rsp_vs_pt_%s.pdf" % (eta_bin)))
+            # entries = []
+            # for fdict in entry_dicts:
+            #     entry = deepcopy(fdict)
+            #     entry["graph"] = construct_inverse_graph(cu.grab_obj_from_file(args.input, "%s/%s_%s" % (mydir, fdict['flav'], eta_bin)))
+            #     entry["line_color"] = fdict['colour']
+            #     entry["marker_color"] = fdict['colour']
+            #     entries.append(entry)
+            # bin_title = eta_bin.replace("to", " < |#eta| < ").replace("JetEta", "")
+            # do_comparison_graph(entries, bin_title=bin_title,
+            #                     xtitle="p_{T}^{Gen} [GeV]", ytitle="1/Response", logx=True,
+            #                     y_limit_protection=(0.8, 1.2),
+            #                     other_elements=other_elements,
+            #                     output_filename=os.path.join(plot_dir, "inv_rsp_vs_pt_%s.pdf" % (eta_bin)))
 
         # Do all flavs rsp vs eta for given pt bin
-        common_pt_bins = cu.get_common_pt_bins(obj_list)
-        for pt_bin in common_pt_bins:
-            entries = []
-            for fdict in entry_dicts:
-                entry = deepcopy(fdict)
-                entry["graph"] = cu.grab_obj_from_file(args.input, "%s/%s_%s" % (mydir, fdict['flav'].replace("RefPt", "JetEta"), pt_bin))
-                entry["line_color"] = fdict['colour']
-                entry["marker_color"] = fdict['colour']
-                entries.append(entry)
-            bin_title = pt_bin.replace("to", " < p_{T} < ").replace("RefPt", "")
-            do_comparison_graph(entries, bin_title=bin_title + " GeV",
-                                xtitle="|#eta|", ytitle="Response",
-                                xlimits=(0, 5.2), y_limit_protection=(0.5, 1.5),
-                                other_elements=other_elements,
-                                output_filename=os.path.join(plot_dir, "rsp_vs_eta_%s.pdf" % (pt_bin)))
+        # common_pt_bins = cu.get_common_pt_bins(obj_list)
+        # for pt_bin in common_pt_bins:
+        #     entries = []
+        #     contributions = []
+        #     for fdict in entry_dicts:
+        #         entry = deepcopy(fdict)
+        #         # entry["graph"] = cu.grab_obj_from_file(args.input, "%s/%s_%s" % (mydir, fdict['flav'].replace("RefPt", "JetEta"), pt_bin))
+        #         entry["obj"] = cu.grab_obj_from_file(args.input, "%s/%s_%s" % (mydir, fdict['flav'].replace("RefPt", "JetEta"), pt_bin))
+        #         entry["line_color"] = fdict['colour']
+        #         entry["marker_color"] = fdict['colour']
+        #         # entries.append(entry)
+
+        #         del entry['flav']
+        #         del entry['colour']
+        #         this_contrib = Contribution(**entry)
+        #         contributions.append(this_contrib)
+            
+        #     bin_title = pt_bin.replace("to", " < p_{T} < ").replace("RefPt", "")
+        #     do_comparison_graph(contributions, bin_title=bin_title + " GeV",
+        #                         xtitle="|#eta|", ytitle="Response",
+        #                         xlimits=(0, 5.2), y_limit_protection=(0.5, 1.5),
+        #                         other_elements=other_elements,
+        #                         output_filename=os.path.join(plot_dir, "rsp_vs_eta_%s.pdf" % (pt_bin)))
 
             # Inverse response ie ~ correction
             # entries = []
